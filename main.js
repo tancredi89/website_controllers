5cd06ecfdf6f16098764c1d453bcb490ece5d367
tancredi89 <tancredi.busatta@gmail.com>
2020-10-19 12:03:32 +0200

    sound + time

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function showMenu() {
     if (document.querySelector('.menu').style.visibility == "visible") {
-        (document.querySelector('.menu').style.visibility = "hidden")
+        (document.querySelector('.menu').style.visibility = "hidden");
     } else { 
-        (document.querySelector('.menu').style.visibility = "visible")
+        (document.querySelector('.menu').style.visibility = "visible");
 
     }
 };
@@ -11,9 +11,42 @@ function showMenu() {
 
 function showMetop() {
     if (document.querySelector('.fullMetop').style.visibility == "visible") {
-        (document.querySelector('.fullMetop').style.visibility = "hidden")
+        (document.querySelector('.fullMetop').style.visibility = "hidden");
     } else { 
-        (document.querySelector('.fullMetop').style.visibility = "visible")
+        (document.querySelector('.fullMetop').style.visibility = "visible");
 
     }
+};
+
+
+function displayTime() {
+    var day = new Date();
+    var s = day.getSeconds();
+    var m = day.getMinutes();
+    var h = day.getHours();
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var stringTime = "UTC  "+ (h-2) + ":" + m + ":" + s;
+    
+    document.getElementById("time").innerText = stringTime;
+    document.getElementById("time").textContent = stringTime;
+    
+    setTimeout(displayTime, 1000);
 }
+displayTime();
+
+function soundChange() {
+    var soundText = document.getElementById('sound').innerHTML;
+    
+    if (soundText == "SOUND OFF") {
+        var audio = document.getElementById('soundOff')
+        document.getElementById('sound').innerHTML = 'SOUND ON';
+    } else {
+        document.getElementById('sound').innerHTML = 'SOUND OFF';
+        var audio = document.getElementById('soundOn');
+    }
+    audio.play();
+};
---
+++ test.py
@@ -0,0 +1,5 @@
+import mysql.connector
+
+cnx = mysql.connector.connect(user='epscot', password='*707F22A7CDFA063D7C291A3E8236E0085E02487C',
+                              host='localhost',
+                              database='epscot')
