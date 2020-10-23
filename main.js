5a0514605eb740be1d4aa5d637db04cfdbd7f9ab
tancredi89 <tancredi.busatta@gmail.com>
2020-10-23 19:43:36 +0200

    update countdown

--- main.js
+++ main.js
@@ -20,12 +20,16 @@ function showMetop() {
 
 function displayTime() {
     var day = new Date();
-    var s = day.getSeconds();
-    var m = day.getMinutes();
-    var h = day.getHours()-2;
-    var metopAAos = "22:03:46";
-    var metopBAos = "10:04:10";
-    var metopCAos = "04:58:26";
+    var s = parseInt(day.getSeconds());
+    var m = parseInt(day.getMinutes());
+    var h = parseInt(day.getHours())-2;
+    var data = parseInt(day.getDate());
+    var mese = parseInt(day.getMonth());
+    var DOY = dayOfYear(data,mese);
+
+    var metopAAos = "298:13:28:46";
+    var metopBAos = "299:10:04:10";
+    var metopCAos = "300:13:18:26";
 
     var metopA = timerCountdown(metopAAos)[0];
     var metopB = timerCountdown(metopBAos)[0];
@@ -36,7 +40,6 @@ function displayTime() {
     var deltaC = timerCountdown(metopCAos)[1];
 
     if (deltaA>deltaB && deltaB>deltaC) {
-        console.log("CBA");
         document.getElementById("metopc").style.zIndex = "3";
         document.getElementById("metopc").style.top = "150px";
         document.getElementById("metopc").style.left = "150px";
@@ -55,7 +58,6 @@ function displayTime() {
         document.getElementById("fullmetopa").style.gridColumn = "1 / span 1"
 
     } else if (deltaC>deltaA && deltaA>deltaB) {
-        console.log("BAC");
         document.getElementById("metopb").style.zIndex = "3";
         document.getElementById("metopb").style.top = "150px";
         document.getElementById("metopb").style.left = "150px";
@@ -73,7 +75,6 @@ function displayTime() {
         document.getElementById("fullmetopc").style.gridRow = "4 / span 1"
         document.getElementById("fullmetopc").style.gridColumn = "1 / span 1"
     } else if (deltaB>deltaC && deltaC>deltaA) {
-        console.log("ACB");
         document.getElementById("metopb").style.zIndex = "1";
         document.getElementById("metopb").style.top = "50px";
         document.getElementById("metopb").style.left = "50px";
@@ -99,35 +100,35 @@ function displayTime() {
     var stringTime = "UTC  "+ h + ":" + m + ":" + s;
     
     document.getElementById("time").textContent = stringTime;
-    document.getElementById("metopa-cntdwn").textContent = "- "+metopA;
-    document.getElementById("metopb-cntdwn").textContent = "- "+metopB;
-    document.getElementById("metopc-cntdwn").textContent = "- "+metopC;
-    document.getElementById("fullmetopa-cntdwn").textContent = "- "+metopA;
-    document.getElementById("fullmetopb-cntdwn").textContent = "- "+metopB;
-    document.getElementById("fullmetopc-cntdwn").textContent = "- "+metopC;
+    document.getElementById("metopa-cntdwn").textContent = metopA;
+    document.getElementById("metopb-cntdwn").textContent = metopB;
+    document.getElementById("metopc-cntdwn").textContent = metopC;
+    document.getElementById("m02aos").textContent = "AOS "+metopAAos.slice(4,12);
+    document.getElementById("m01aos").textContent = "AOS "+metopBAos.slice(4,12);
+    document.getElementById("m03aos").textContent = "AOS "+metopCAos.slice(4,12);
 
-    var soundPlayed = false
+    document.getElementById("fullmetopa-cntdwn").textContent = metopA;
+    document.getElementById("fullmetopb-cntdwn").textContent = metopB;
+    document.getElementById("fullmetopc-cntdwn").textContent = metopC;
+    document.getElementById("fullmetopa-cntdwn").textContent = metopA;
+    document.getElementById("fullmetopb-cntdwn").textContent = metopB;
+    document.getElementById("fullmetopc-cntdwn").textContent = metopC;
 
-    if (deltaA<60){
-        var audio = document.getElementById('1minwarning')
-        if (soundPlayed==false){
-            audio.play();
-            soundPlayed=true;
-        }
+    if (Math.abs(deltaA)<50){
+        var audio = document.getElementById('1minwarning');
+        audio.pause();
+    }  else if (Math.abs(deltaA)<60) {
+        var audio = document.getElementById('1minwarning');
+        audio.play();
     }
-    if (deltaB<60){
+
+    if (Math.abs(deltaB)<60){
         var audio = document.getElementById('1minwarning')
-        if (soundPlayed==false){
-            audio.play();
-            soundPlayed=true;
-        }
+        audio.play();
     }
-    if (deltaC<60){
-        var audio = document.getElementById('1minwarning')
-        if (soundPlayed==false){
-            audio.play();
-            soundPlayed=true;
-        }    
+    if (Math.abs(deltaC)<60){
+        var audio = document.getElementById('1minwarning');
+        audio.play();  
     }
 
 
@@ -148,13 +149,42 @@ function soundChange() {
     audio.play();
 };
 
+function dayOfYear(day,mese) {
+    var months = [31,28,31,30,31,30,31,31,30,31,30,31];
+    var DOY = 0;
+    for (var m = 0; m <= mese;m++ ) {
+        if (m == mese-1) {
+            DOY +=day
+        } else {
+            DOY += months[m];
+        }
+    }
+    return DOY
+}
+
+
+//Missing day as a part of the countdown
 function timerCountdown(time) {
     var parsedTime = time.split(":");
-    var s = parsedTime[2];
-    var m = parsedTime[1];
-    var h = parsedTime[0];
+    var s = parseInt(parsedTime[3]);
+    var m = parseInt(parsedTime[2]);
+    var h = parseInt(parsedTime[1]);
+    var metopDOY = parseInt(parsedTime[0]);
     var now = new Date();
-    var hours = now.getHours()-2;
+    var todayDate = parseInt(now.getDate());
+    var todayMonth = parseInt(now.getMonth());
+    var todayDOY = dayOfYear(todayDate,todayMonth);
+    var hours = parseInt(now.getHours())-2;
+    var mdown = m - parseInt(now.getMinutes());
+    if (todayDOY==metopDOY) {
+        var hdown = Math.abs(h - hours);
+    } else {
+        var hdown = (todayDOY-metopDOY)*24+h - hours
+    }
+
+    if (mdown<0) {
+        mdown += 60;
+    } 
 
     if (s < now.getSeconds()) {
         var sdown = (parseInt(s)+60)-now.getSeconds();
@@ -162,27 +192,23 @@ function timerCountdown(time) {
         var sdown = s-now.getSeconds();    
     }
 
-    if (m < now.getMinutes()) {
-        var mdown = (parseInt(m)+60)-now.getMinutes();
-    } else {
-        var mdown = m-now.getMinutes();    
+    if (s==now.getSeconds()) {
+        mdown -= 1; 
     }
 
-    if (h < hours) {
-        var hdown = (parseInt(h)+24)-hours;
-    } else {
-        var hdown = h-hours;    
+    if (m==now.getMinutes()) {
+        hdown -= 1;
     }
 
-    var delta = hdown*3600+mdown*60+sdown;
+    var delta = Math.abs(hdown)*3600+Math.abs(mdown)*60+Math.abs(sdown);
 
-    if (hdown < 10) {
+    if (hdown < 10 && hdown > -10) {
         hdown = "0"+hdown;
     }
-    if (mdown < 10) {
+    if (mdown < 10 && mdown > -10) {
         mdown = "0"+mdown;
     }
-    if (sdown < 10) {
+    if (sdown < 10 && sdown > -10) {
         sdown = "0"+sdown;
     }
 
